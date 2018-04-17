2e46a8b09a32809573b2dd64b8a637d274272736
Ayana Bando <ayanaflax@yahoo.com>
2018-04-17 00:36:55 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,8 +59,8 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.split
+  binding.pry
   holiday_hash.each do |season, holiday|
-    binding.pry
     puts "#{season}: #{holiday}"
   end
 end
